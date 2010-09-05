65da7b34fdaa84af892570d7df469b6d44d4af83
Peter Savage <silentkeystroke@googlemail.com>
2010-09-05 15:19:19 +0100

    Moved main nuts and cleaned up

--- ethestra.py
+++ ethestra.py
@@ -12,46 +12,10 @@ fla = 0
 
 
 
-ch1 = 0
-ch2 = 0
-ch3 = 0
-ch4 = 0
-ch5 = 0
-ch12 = 0
-mod1 = 0
-mod2 = 0
-mod3 = 0
-mod4 = 0
-mod5 = 0
-mod12 = 0
 
 def handler(pkt):
     print pkt.summary()
-    """
-    #print pkt.type
-    global seq
-    
-    global arg
-    global ch1, ch2, ch3, ch4, ch5, ch12
-    global mod1, mod2, mod3, mod4, mod5, mod12
-
-    if LLC in pkt:
-		return
 	
-    if pkt.type != 2048:
-		return
-
-    if pkt.proto == 6 or pkt.proto == 17:
-	    if pkt.dport == 5222 or pkt.sport == 5222:
-		print "CHAT"
-		seq.GetChannel(2).pattern.append([random.random()*64, 0x2A, 0x60, 4])
-	"""
-
-	
-def nuts(e):
-	print e.GetChannel(2).pattern
-
-
 
 
 class Ethestra():
@@ -67,7 +31,7 @@ class Ethestra():
 		self.seq.AddChannel(1, name="Totoro")
 		self.seq.AddChannel(2)
 		self.seq.PlayBar()
-		self.seq.connect("bar-fin", nuts)
+		self.seq.connect("bar-fin", self.nuts)
 		Loop = gobject.MainLoop()
 		Loop.run()
 		
@@ -84,6 +48,9 @@ class Ethestra():
 			return True
 		else:
 			return False
+			
+	def nuts(self, e):
+		print e.GetChannel(2).pattern
 	
 
 ethestra = Ethestra()
