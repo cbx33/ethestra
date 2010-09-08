a824a5f6ef32aeb066afcf00a909017b411e345a
Peter Savage <silentkeystroke@googlemail.com>
2010-09-08 23:48:45 +0100

    Added support file for ZynAddSubFX parameters

--- ethestra.py
+++ ethestra.py
@@ -9,5 +9,5 @@ import core
 ethestra = core.Ethestra("ZynAddSubFX", tempo=100)
 ethestra.AddInstrument(1, "ARP Instrument", "IS ARP", pattern=None)
 ethestra.AddInstrument(2, "ARP Instrument", "IS ARP OR IS TCP", pattern=[])
-ethestra.AddInstrument(10, "ARP Instrument", "IS TCP", pattern=[])
+ethestra.AddInstrument(10, "ARP Instrument", "IS TCP", pattern=None)
 ethestra.Start()
